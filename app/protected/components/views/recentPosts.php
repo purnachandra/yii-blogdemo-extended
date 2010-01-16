6f3f65e8a0f2373ce7f8fb0b69a97cab58ab9aeb
rasmus <rasmus@mindplay.dk>
2010-01-16 17:08:10 +0000

    use permalinks in recent posts

--- app/protected/components/views/recentPosts.php
+++ app/protected/components/views/recentPosts.php
@@ -1,7 +1,7 @@
 <ul>
 <?php foreach($this->getRecentPosts() as $post): ?>
 <li>
-<?php echo CHtml::link(CHtml::encode($post->title),array('post/view','id'=>$post->id)); ?>
+<?php echo CHtml::link(CHtml::encode($post->title),$post->url); ?>
 &nbsp;on&nbsp;
 <?php echo CHTml::link(date('M j', $post->create_time), array('post/PostedOnDate', 'time'=>$post->create_time)); ?>
 </li>
