5daff6561c76c707a74ef9ff3f1374ff893a8aae
rasmus <rasmus@mindplay.dk>
2010-01-17 00:07:26 +0000

    add image manager application component; add temporary image controller for testing

---
+++ app/protected/controllers/ImageController.php
@@ -0,0 +1,20 @@
+<?php
+
+class ImageController extends Controller
+{
+  public function actionTest()
+  {
+    // this is a simple demonstration of the image manager.
+    // collections and transformations are defined in the main configuration file.
+    
+    $name = 'test.jpg';
+    
+    // load the image 'test.jpg' from the configured collection named 'full':
+    $image = Yii::app()->images->full->load($name);
+    
+    // apply the transformation named 'thumbnail' and save the image:
+    $image->thumbnail()->saveJPEG('images/thumbs/'.$name);
+    
+    echo '<img src="/images/thumbs/'.$name.'"/>';
+  }
+}
