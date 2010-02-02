6c3907b2c21e357bf2eecff5a4fc0a28c154594f
mocapapa <mocapapa@g.pugpug.org>
2010-02-02 01:48:16 +0000

    updated according to the original of r1779

--- app/index-test.php
+++ app/index-test.php
@@ -8,7 +8,7 @@
 $yii=dirname(dirname(__FILE__)).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 // remove the following line when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+// defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
